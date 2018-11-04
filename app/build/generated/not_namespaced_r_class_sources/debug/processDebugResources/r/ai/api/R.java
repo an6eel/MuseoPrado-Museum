940a40ab551da690d2bf72d2b0150db059c87d89
an6eel <agarciamal1997@gmail.com>
2018-11-04 14:32:01 +0100

    v3

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/ai/api/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/ai/api/R.java
@@ -12,26 +12,26 @@ public final class R {
     public static final class attr {
         private attr() {}
 
-        public static final int centerColor = 0x7f030061;
-        public static final int circleCenterX = 0x7f03007b;
-        public static final int circleCenterY = 0x7f03007c;
-        public static final int haloColor = 0x7f0300eb;
-        public static final int mainColor = 0x7f030167;
-        public static final int maxRadius = 0x7f03016d;
-        public static final int minRadius = 0x7f030172;
-        public static final int state_initializing_tts = 0x7f0301b3;
-        public static final int state_listening = 0x7f0301b6;
-        public static final int state_speaking = 0x7f0301b7;
-        public static final int state_waiting = 0x7f0301b8;
+        public static final int centerColor = 0x7f030062;
+        public static final int circleCenterX = 0x7f03007c;
+        public static final int circleCenterY = 0x7f03007d;
+        public static final int haloColor = 0x7f0300ee;
+        public static final int mainColor = 0x7f03016c;
+        public static final int maxRadius = 0x7f030172;
+        public static final int minRadius = 0x7f030177;
+        public static final int state_initializing_tts = 0x7f0301b9;
+        public static final int state_listening = 0x7f0301bc;
+        public static final int state_speaking = 0x7f0301bd;
+        public static final int state_waiting = 0x7f0301be;
     }
     public static final class color {
         private color() {}
 
         public static final int aidialog_background = 0x7f05001a;
-        public static final int icon_orange_color = 0x7f050050;
-        public static final int main_bg_color = 0x7f050052;
-        public static final int mic_button_halo = 0x7f05005f;
-        public static final int mic_colors = 0x7f050060;
+        public static final int icon_orange_color = 0x7f05005b;
+        public static final int main_bg_color = 0x7f05005d;
+        public static final int mic_button_halo = 0x7f05006a;
+        public static final int mic_colors = 0x7f05006b;
     }
     public static final class dimen {
         private dimen() {}
@@ -43,19 +43,19 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int cube = 0x7f07005d;
-        public static final int mic_control = 0x7f07007d;
-        public static final int microphone_control = 0x7f07007e;
-        public static final int speaker = 0x7f070091;
-        public static final int speaker_silent = 0x7f070092;
+        public static final int cube = 0x7f070071;
+        public static final int mic_control = 0x7f070096;
+        public static final int microphone_control = 0x7f070097;
+        public static final int speaker = 0x7f0700ac;
+        public static final int speaker_silent = 0x7f0700ad;
     }
     public static final class id {
         private id() {}
 
-        public static final int micButton = 0x7f080072;
-        public static final int micContainer = 0x7f080073;
-        public static final int partialResultsTextView = 0x7f080088;
-        public static final int titleTextView = 0x7f0800d1;
+        public static final int micButton = 0x7f08007a;
+        public static final int micContainer = 0x7f08007b;
+        public static final int partialResultsTextView = 0x7f080094;
+        public static final int titleTextView = 0x7f0800df;
     }
     public static final class layout {
         private layout() {}
@@ -71,7 +71,7 @@ public final class R {
         private string() {}
 
         public static final int app_name = 0x7f0f0029;
-        public static final int default_aidialog_title = 0x7f0f0030;
+        public static final int default_aidialog_title = 0x7f0f0042;
     }
     public static final class style {
         private style() {}
@@ -83,9 +83,9 @@ public final class R {
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] MaskedColorView = { 0x7f030167 };
+        public static final int[] MaskedColorView = { 0x7f03016c };
         public static final int MaskedColorView_mainColor = 0;
-        public static final int[] SoundLevelButton = { 0x7f030061, 0x7f03007b, 0x7f03007c, 0x7f0300eb, 0x7f03016d, 0x7f030172, 0x7f0301b3, 0x7f0301b6, 0x7f0301b7, 0x7f0301b8 };
+        public static final int[] SoundLevelButton = { 0x7f030062, 0x7f03007c, 0x7f03007d, 0x7f0300ee, 0x7f030172, 0x7f030177, 0x7f0301b9, 0x7f0301bc, 0x7f0301bd, 0x7f0301be };
         public static final int SoundLevelButton_centerColor = 0;
         public static final int SoundLevelButton_circleCenterX = 1;
         public static final int SoundLevelButton_circleCenterY = 2;
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/pl/droidsonroids/gif/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/pl/droidsonroids/gif/R.java
@@ -12,18 +12,18 @@ public final class R {
     public static final class attr {
         private attr() {}
 
-        public static final int freezesAnimation = 0x7f0300e7;
-        public static final int gifSource = 0x7f0300e9;
-        public static final int isOpaque = 0x7f030108;
-        public static final int loopCount = 0x7f030166;
+        public static final int freezesAnimation = 0x7f0300ea;
+        public static final int gifSource = 0x7f0300ec;
+        public static final int isOpaque = 0x7f03010d;
+        public static final int loopCount = 0x7f03016b;
     }
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] GifTextureView = { 0x7f0300e9, 0x7f030108 };
+        public static final int[] GifTextureView = { 0x7f0300ec, 0x7f03010d };
         public static final int GifTextureView_gifSource = 0;
         public static final int GifTextureView_isOpaque = 1;
-        public static final int[] GifView = { 0x7f0300e7, 0x7f030166 };
+        public static final int[] GifView = { 0x7f0300ea, 0x7f03016b };
         public static final int GifView_freezesAnimation = 0;
         public static final int GifView_loopCount = 1;
     }
